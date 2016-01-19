23c391aeb8979b8b2fec44af8d1846a7cac56e76
AchmadGozali8 <achmadg6@gmail.com>
2016-01-19 23:12:44 +0800

    Update models.py

--- Polling-Apps/models.py
+++ Polling-Apps/models.py
@@ -1,22 +1,18 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+db = SQLAlchemy(app)
 
 class User(db.Model):
-	'''
-	Set Table Name
-	'''
+
 	__tablename__ = "user_login"
 
 	#fields
 	id = db.Column(db.Integer,primary_key=True)
-	nama_lengkap = db.Column(db.String(120))
+	full_name=db.Column(db.String(120))
 	email = db.Column(db.String(120), unique=True)
 
-	def __init__(self, nama_lengkap,email):
-
-		self.nama_lengkap= nama_lengkap
+	def __init__(self, full_name,email):
+		self.full_name = full_name
 		self.email = email
 	def __repr__(self):
-		return "<User {}".format(self.nama_lengkap)
+		return "<User {}".format(self.full_name)
