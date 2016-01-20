70ea32578d9280e9fe2646edf736ccc871e9e378
AchmadGozali8 <achmadg6@gmail.com>
2016-01-20 14:36:14 +0800

    Update config.py

--- Polling-Apps/config.py
+++ Polling-Apps/config.py
@@ -5,5 +5,5 @@ DEBUG = True
 APP_DIR = os.path.abspath(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR,os.pardir))
 
-SQLALCHMEY_DATABASE_URI='sqlite:///' + PROJECT_ROOT+'apps.db'
+SQLALCHEMY_DATABASE_URI='sqlite:///' + PROJECT_ROOT+'apps.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
