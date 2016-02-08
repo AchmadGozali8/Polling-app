34a1830d4b439a95583b2d253484f933e69904c9
AchmadGozali8 <achmadg6@gmail.com>
2016-02-08 21:12:20 +0800

    Update app.py

--- Polling-Apps/app.py
+++ Polling-Apps/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, session,redirect,render_template,request,url_for
-from models import User,db
+from models import User,db,Pilihan
 import sys
+
 
 app=Flask(__name__)
 app.config.from_object('config')
@@ -15,22 +16,45 @@ def register():
 	if request.method=="POST":
 		nama_lengkap = request.form.get("nama_lengkap", None)
 		email = request.form.get("email",None)
-		if not validation([nama_lengkap, email]):
-			error = 'too stupid'
-			return render_template("login.html",**locals())
-
+		if nama_lengkap == "" or email == "":
+			error = 'masukkan nama lengkap dan email secara benar'
+			return render_template("registrasi.html",**locals())
 		user = User(nama_lengkap,email)
 		db.session.add(user)
 		db.session.commit()
-		return 'something'
+		#simpan user.id di session
+		session["user_id"]=user.id
+		#membatasi bnyaknya user yang memiliha
+		if user.id ==11:
+			return redirect("/hasil")
+		elif user.id > 10:
+			error = "user yang melakukan polling sudah cukup"
+			return render_template("registrasi.html", **locals())
+		return redirect("/polling")
+	return render_template("registrasi.html", **locals())
 
-	return render_template("login.html", **locals())
+@app.route('/polling',methods=["POST","GET"])
+def polling():
+	if request.method=="POST":
+		python = request.form.get("python",None)
+		go = request.form.get("go",None)
+		user_id= session.get("user_id",None)
+		if python  == "python":
+			polls = Pilihan(user_id,python)
+			db.session.add(polls)
+			db.session.commit()
+		else:
+			polls = Pilihan(user_id,go)
+			db.session.add(polls)
+			db.session.commit()
+		return redirect("/")
+	return render_template("polling.html")
 
-def validation(fields):
-	for loop in fields:
-		if loop is None or loop == "":
-			return False
+@app.route("/hasil")
+def hasil():
+	user = User.query.all()
+	polls=Pilihan.query.all()
+	return render_template("hasil.html", **locals())
 
-	return True
 if __name__=="__main__":
 	app.run()
