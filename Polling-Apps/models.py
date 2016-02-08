d443c3d9590e0e4d089c3340888717e438c055ed
AchmadGozali8 <achmadg6@gmail.com>
2016-02-08 21:15:36 +0800

    Update models.py

--- Polling-Apps/models.py
+++ Polling-Apps/models.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -11,8 +10,25 @@ class User(db.Model):
 	nama_lengkap = db.Column(db.String(120))
 	email = db.Column(db.String(120), unique=True)
 
-	def __init__(self, nama_lengkap,email):
+	def __init__(self, nama_lengkap, email):
 		self.nama_lengkap = nama_lengkap
 		self.email = email
+
 	def __repr__(self):
-		return "<User {}".format(self.nama_lengkap)
+		return "<User {}>".format(self.nama_lengkap)
+class Pilihan(db.Model):
+	__tablename__="pilihan"
+
+	#fields
+	id = db.Column(db.Integer,primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey("user_login.id"))
+	user = db.relationship('User',
+			backref=db.backref("pilihan_user",lazy="dynamic"))
+	pilihan = db.Column(db.String(180))
+
+	def __init__(self,user_id, pilihan):
+		self.user_id=user_id
+		self.pilihan=pilihan
+
+	def __repr__(self):
+		return"<pilihan{}>".format(self.pilihan)
