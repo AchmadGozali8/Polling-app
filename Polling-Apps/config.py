a229c66f65a8c912b0762e34fa2e576aeef625ea
AchmadGozali8 <achmadg6@gmail.com>
2016-02-08 21:13:38 +0800

    Update config.py

--- Polling-Apps/config.py
+++ Polling-Apps/config.py
@@ -1,9 +1,7 @@
 '''configuration'''
 import os
-
 DEBUG = True
-APP_DIR = os.path.abspath(os.path.dirname(__file__))
-PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR,os.pardir))
-
-SQLALCHEMY_DATABASE_URI='sqlite:///' + PROJECT_ROOT+'apps.db'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__name__))
+SQLALCHEMY_DATABASE_URI= 'mysql://root:ichal@localhost/apps'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+SECRET_KEY = '8a0d87868f004541a99c6f6bddf4f9d5'
